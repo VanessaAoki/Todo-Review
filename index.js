54f206caa231d27f64c2f577cde7fb7050eb656a
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-08-23 18:02:53 -0300

    Remove files

--- index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
-import './style.css';
-import './checkbox';
-import './todo';
